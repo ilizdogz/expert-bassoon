cec4c71a11b5426eadf1b21990186fbce8e17c4f
Krzysztof Glimos <kglimos@gmail.com>
2019-07-05 21:16:30 +0200

    CORS

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env/python
 from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 # import RPi.GPIO as GPIO
 import time
 from transition import transition
 
 app = Flask(__name__)
+CORS(app)
 
 currentColor = [0, 0, 0]
 FPS = 30
